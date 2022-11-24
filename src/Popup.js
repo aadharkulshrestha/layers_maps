b6ae1f051049748157e9ab13b4fde788c655d2e7
aadharkulshrestha <deepakulshrestha13@gmail.com>
2022-11-24 14:34:10 +0530

    Popup Done

--- src/Popup.js
+++ src/Popup.js
@@ -3,43 +3,43 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  makeStyles,
   Typography,
   IconButton,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import DoneAllIcon from "@mui/icons-material/DoneAll";
-import HelpOutlineTwoToneIcon from '@mui/icons-material/HelpOutlineTwoTone';
+import HelpOutlineTwoToneIcon from "@mui/icons-material/HelpOutlineTwoTone";
 
 const useStyles = makeStyles((theme) => ({
-  dialog: {
-    padding: theme.spacing(2),
-    position: "absolute",
-    top: theme.spacing(5),
-  },
-  dialogTitle: {
-    textAlign: "center",
-  },
-  dialogContent: {
-    textAlign: "center",
-  },
-  dialogAction: {
-    justifyContent: "center",
-  },
-  titleIcon: {
-    backgroundColor: theme.palette.primary.light,
-    color: theme.palette.primary.main,
-    "&:hover": {
-      backgroundColor: theme.palette.primary.light,
-      cursor: "default",
-    },
-    "& .MuiSvgIcon-root": {
-      fontSize: "7rem",
-    },
-  },
+  // dialog: {
+  //   padding: 10,
+  //   position: "absolute",
+  //   top: theme.spacing(5),
+  // },
+  // dialogTitle: {
+  //   textAlign: "center",
+  // },
+  // dialogContent: {
+  //   textAlign: "center",
+  // },
+  // dialogAction: {
+  //   justifyContent: "center",
+  // },
+  // titleIcon: {
+  //   backgroundColor: theme.palette.primary.light,
+  //   color: theme.palette.primary.main,
+  //   "&:hover": {
+  //     backgroundColor: theme.palette.primary.light,
+  //     cursor: "default",
+  //   },
+  //   "& .MuiSvgIcon-root": {
+  //     fontSize: "7rem",
+  //   },
+  // },
 }));
 
 export default function Popup(props) {
-  const { caller, children, openPopup, setOpenPopup } = props;
+  const { children, openPopup, setOpenPopup } = props;
   const classes = useStyles();
 
   return (
@@ -49,15 +49,9 @@ export default function Popup(props) {
       classes={{ paper: classes.dialogWrapper }}
     >
       <DialogTitle className={classes.dialogTitle}>
-        {caller == "login" ? (
-          <IconButton disableRipple className={classes.titleIcon}>
-            <HelpOutlineTwoToneIcon />
-          </IconButton>
-        ) : (
-          <IconButton disableRipple className={classes.titleIcon}>
-            <DoneAllIcon />
-          </IconButton>
-        )}
+        {/* <IconButton disableRipple className={classes.titleIcon}>
+          <DoneAllIcon />
+        </IconButton> */}
       </DialogTitle>
       <DialogContent>{children}</DialogContent>
     </Dialog>
